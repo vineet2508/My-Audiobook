5f2a19abcc06252a571ea6a1b2274d01ea7805fa
Vineet Raj Parashar <vineetrajparashar25@gmail.com>
2020-11-11 15:36:23 +0530

    Add files via upload

---
+++ Audiobook.py
@@ -0,0 +1,25 @@
+#Importing the libraries
+#You need to install both of these modules using pip command
+import pyttsx3
+import PyPDF2
+book=open('.....file_path\\Filename.pdf','rb')
+pdfReader=PyPDF2.PdfFileReader(book)
+pages=pdfReader.numPages
+print("Total no. of Pages inside PDF:"+ str(pages))
+speaker= pyttsx3.init()
+#For reading a particular pages you just need to give the it's previous pages number.
+pnum=int(input("Enter the page number :"))
+page = pdfReader.getPage(pnum)
+text = page.extractText()
+speaker.say(text)
+print("Reading...")
+speaker.runAndWait()
+
+#for reading full pdf
+'''for num in range(7,pages):
+    print('Reading on page no.: ',7+num)
+    page=pdfReader.getPage(7+num)
+    text=page.extractText()
+    speaker.say(text)
+    print("Reading...")
+    speaker.runAndWait()'''
